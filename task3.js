2e7f73007fb386e04e98fbee7effb6a1942a3a1c
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-26 18:33:18 +0700

    Update task3.js
    пофиксил

--- task3.js
+++ task3.js
@@ -9,15 +9,16 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
+	const delay = randomIntegerFromInterval(200, 500);
+
 	return new Promise((resolve, reject)=>{
 		
-		const delay = randomIntegerFromInterval(200, 500);
 
 		setTimeout(() => {
 		  const canProcess = Math.random() > 0.3;
 	  
 		  if (canProcess) {
-			  resolve(transaction.id)
+			  resolve({id: transaction.id, time: delay});
 		  //   onSuccess(transaction.id, delay);
 		  } else {
 			  reject(transaction.id)
@@ -28,7 +29,7 @@ const makeTransaction = (transaction) => {
   
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({id, time}) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
